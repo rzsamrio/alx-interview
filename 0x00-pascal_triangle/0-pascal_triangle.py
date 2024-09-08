4de573ea6e18f69e72e5676b5760106a875059f1
rzsamrio <samuelekop144@gmail.com>
2024-09-08 08:21:38 +0100

    [0x00] optimized

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+""" Simple python module contains Pascal Triangle function """
+
+
 def pascal_triangle(n):
-    triangle = []
+    """ Compute pascal's triangle """
 
+    final = []
     if (n <= 0):
-        return triangle
+        return []
 
+    triangle = []
     for row in range(n):
         oldtriangle = triangle.copy()
         for pos in range(len(triangle)):
             if pos > 0:
                 triangle[pos] = oldtriangle[pos] + oldtriangle[pos - 1]
         triangle.append(1)
-        display = ','.join(map(str, triangle))
-        print(f'[{display}]')
+        final.append(triangle.copy())
+    return final
